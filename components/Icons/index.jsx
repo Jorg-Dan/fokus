b215063f2388f4fc877c08e629e419a0822cdaf5
Jorge Daniel <jorgedaniel.camelo@gmail.com>
2025-10-24 15:06:01 -0300

    x

--- components/Icons/index.jsx
+++ components/Icons/index.jsx
@@ -1,7 +1,7 @@
 import Svg, { Path } from 'react-native-svg';
 
-export const IconPlay = () => {
-    return (
+    export const IconPlay = () => {
+       return (
         <Svg width="11" height="15" viewBox="0 0 11 15" fill="none">
             <Path d="M0.015625 0.473022L10.9844 7.4574L0.015625 14.4418V0.473022Z" fill="#021123" />
         </Svg>
